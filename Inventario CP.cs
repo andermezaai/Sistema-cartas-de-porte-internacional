83508214c5cda1f5d9dbad0cd1b7358b7f53878d
Anderson Meza <mezacando.anderson@gmail.com>
2026-07-25 18:55:06 -0500

    Se actualizan cambios

--- Inventario CP.cs
+++ Inventario CP.cs
@@ -769,146 +769,147 @@ namespace Documentos
         }
 
         private void pictureBox1_Click(object sender, EventArgs e)
-        {                                  
-            Base nueva = new Base();
-            refrescar_valores();
+        {
             
-            string busquedaEcu = "Select numero_cartaporte from cartas_de_porte where codigo_pais='EC' Order By numero_cartaporte asc";
-            string busquedaCOL = "Select numero_cartaporte from cartas_de_porte where codigo_pais='CO' Order By numero_cartaporte asc";
-            string busquedaPE = "Select numero_cartaporte from cartas_de_porte where codigo_pais='PE' Order By numero_cartaporte asc";
+                Base nueva = new Base();
+                refrescar_valores();
 
-            if(nueva.Consulta(busquedaEcu).Rows.Count!=0)
-            {
-                numeroEC = Convert.ToString(Convert.ToInt32(nueva.Consulta(busquedaEcu).Rows[nueva.Consulta(busquedaEcu).Rows.Count - 1].ItemArray[0]) + 1);
-            }
+                string busquedaEcu = "Select numero_cartaporte from cartas_de_porte where codigo_pais='EC' Order By numero_cartaporte asc";
+                string busquedaCOL = "Select numero_cartaporte from cartas_de_porte where codigo_pais='CO' Order By numero_cartaporte asc";
+                string busquedaPE = "Select numero_cartaporte from cartas_de_porte where codigo_pais='PE' Order By numero_cartaporte asc";
 
-            if (nueva.Consulta(busquedaCOL).Rows.Count != 0)
-            {
-                numeroCOL = Convert.ToString(Convert.ToInt32(nueva.Consulta(busquedaCOL).Rows[nueva.Consulta(busquedaCOL).Rows.Count - 1].ItemArray[0]) + 1);
-            }
-
-            if (nueva.Consulta(busquedaPE).Rows.Count != 0)
-            {
-                numeroPE = Convert.ToString(Convert.ToInt32(nueva.Consulta(busquedaPE).Rows[nueva.Consulta(busquedaPE).Rows.Count - 1].ItemArray[0]) + 1);
-            }
-
-
-            if (!string.IsNullOrEmpty(richTextBox1.Text) && !string.IsNullOrEmpty(richTextBox2.Text) && (comboBox1.Text=="EC" || comboBox1.Text == "CO" || comboBox1.Text == "PE"))
-            {
-                if (!nueva.Dato_en_consulta(c2.Trim(), "Select c2yc3 from Organizaciones_y_direcciones"))
+                if (nueva.Consulta(busquedaEcu).Rows.Count != 0)
                 {
-                    string comando1 = "INSERT INTO Organizaciones_y_direcciones(c2yc3) " +
-                    "VALUES('" + c2.Trim() + "')";
-                    nueva.comando(comando1);
+                    numeroEC = Convert.ToString(Convert.ToInt32(nueva.Consulta(busquedaEcu).Rows[nueva.Consulta(busquedaEcu).Rows.Count - 1].ItemArray[0]) + 1);
                 }
-                string codigo_emisor = nueva.Quitar_espacios(Convert.ToString(nueva.Consulta(@"SELECT Organizaciones_y_direcciones.id_organizacion FROM Organizaciones_y_direcciones WHERE  Replace(c2yc3, Chr (10), ' ') ='" + c2.Trim().Replace('\n',' ') + "'").Rows[0].ItemArray[0]));                
 
-                if (!nueva.Dato_en_consulta(c3.Trim(), "Select c2yc3 from Organizaciones_y_direcciones"))
+                if (nueva.Consulta(busquedaCOL).Rows.Count != 0)
                 {
-                    string comando2 = "INSERT INTO Organizaciones_y_direcciones(c2yc3) " +
-                    "VALUES('" + c3.Trim() + "')";
-                    nueva.comando(comando2);
+                    numeroCOL = Convert.ToString(Convert.ToInt32(nueva.Consulta(busquedaCOL).Rows[nueva.Consulta(busquedaCOL).Rows.Count - 1].ItemArray[0]) + 1);
                 }
-                string codigo_receptor = nueva.Quitar_espacios(Convert.ToString(nueva.Consulta(@"SELECT Organizaciones_y_direcciones.id_organizacion FROM Organizaciones_y_direcciones WHERE  Replace(c2yc3, Chr (10), ' ') ='" + c3.Trim().Replace('\n', ' ') + "'").Rows[0].ItemArray[0]));
 
-                if (id == "-1")
+                if (nueva.Consulta(busquedaPE).Rows.Count != 0)
                 {
-                    string numero = "";
-                    if (comboBox1.Text == "EC")
-                        numero = numeroEC;
-                    else if (comboBox1.Text == "CO")
-                        numero = numeroCOL;
+                    numeroPE = Convert.ToString(Convert.ToInt32(nueva.Consulta(busquedaPE).Rows[nueva.Consulta(busquedaPE).Rows.Count - 1].ItemArray[0]) + 1);
+                }
+
+
+                if (!string.IsNullOrEmpty(richTextBox1.Text) && !string.IsNullOrEmpty(richTextBox2.Text) && (comboBox1.Text == "EC" || comboBox1.Text == "CO" || comboBox1.Text == "PE"))
+                {
+                    if (!nueva.Dato_en_consulta(c2.Trim(), "Select c2yc3 from Organizaciones_y_direcciones"))
+                    {
+                        string comando1 = "INSERT INTO Organizaciones_y_direcciones(c2yc3) " +
+                        "VALUES('" + c2.Trim() + "')";
+                        nueva.comando(comando1);
+                    }
+                    string codigo_emisor = nueva.Quitar_espacios(Convert.ToString(nueva.Consulta(@"SELECT Organizaciones_y_direcciones.id_organizacion FROM Organizaciones_y_direcciones WHERE  [c2yc3] = '" + c2.Trim().ToString().Replace('\n', (char)10) + "'").Rows[0].ItemArray[0]));
+
+                    if (!nueva.Dato_en_consulta(c3.Trim(), "Select c2yc3 from Organizaciones_y_direcciones"))
+                    {
+                        string comando2 = "INSERT INTO Organizaciones_y_direcciones(c2yc3) " +
+                        "VALUES('" + c3.Trim() + "')";
+                        nueva.comando(comando2);
+                    }
+                    string codigo_receptor = nueva.Quitar_espacios(Convert.ToString(nueva.Consulta(@"SELECT Organizaciones_y_direcciones.id_organizacion FROM Organizaciones_y_direcciones WHERE  [c2yc3] = '" + c3.Trim().ToString().Replace('\n', (char)10) + "'").Rows[0].ItemArray[0]));
+
+                    if (id == "-1")
+                    {
+                        string numero = "";
+                        if (comboBox1.Text == "EC")
+                            numero = numeroEC;
+                        else if (comboBox1.Text == "CO")
+                            numero = numeroCOL;
+                        else
+                            numero = numeroPE;
+
+                        string comando3 = "INSERT INTO cartas_de_porte (fecha_creacion,fecha_modificacion,codigo_pais,numero_cartaporte,c4,c5,c6,c7,c8,c9,c10,c11,c12) " +
+                        "VALUES(NOW(), NOW(), '" + comboBox1.Text + "', '" + numero + "', '" + c4 + "', '" + c5 + "', '" +
+                        c6 + "','" + c7 + "','" + c8 + "','" + c9 + "','" + c10 + "','" + c11 + "','" + c12 + "')";
+                        nueva.comando(comando3);
+                        numero_final_para_enviar_al_manifiesto = numero;
+                    }
                     else
-                        numero = numeroPE;
+                    {
+                        string codigo_pais = comboBox1.Text;
+                        string numero = "";
+                        if (comboBox1.Text == "EC")
+                            numero = numeroEC;
+                        else if (comboBox1.Text == "CO")
+                            numero = numeroCOL;
+                        else
+                            numero = numeroPE;
 
-                    string comando3 = "INSERT INTO cartas_de_porte (fecha_creacion,fecha_modificacion,codigo_pais,numero_cartaporte,c4,c5,c6,c7,c8,c9,c10,c11,c12) " +
-                    "VALUES(NOW(), NOW(), '" + comboBox1.Text + "', '" + numero + "', '" + c4 + "', '" + c5 + "', '" +
-                    c6 + "','" + c7 + "','" + c8 + "','" + c9 + "','" + c10 + "','" + c11 + "','" + c12 + "')";
-                    nueva.comando(comando3);
-                    numero_final_para_enviar_al_manifiesto = numero;
-                }
-                else
-                {
-                    string codigo_pais = comboBox1.Text;
-                    string numero = "";                    
-                    if (comboBox1.Text == "EC")
-                        numero = numeroEC;
-                    else if (comboBox1.Text == "CO")
-                        numero = numeroCOL;
+                        DataTable codigopais = nueva.Consulta("Select cartas_de_porte.codigo_pais from cartas_final inner join cartas_de_porte on cartas_final.llave=cartas_de_porte.llave where cartas_final.llave = " + id + "");
+                        if (nueva.Quitar_espacios(Convert.ToString(codigopais.Rows[0].ItemArray[0])) == comboBox1.Text)
+                        {
+                            DataTable numero_cartaporte = nueva.Consulta("Select cartas_de_porte.numero_cartaporte from cartas_final inner join cartas_de_porte on cartas_final.llave=cartas_de_porte.llave where cartas_final.llave = " + id + "");
+                            numero = nueva.Quitar_espacios(Convert.ToString(numero_cartaporte.Rows[0].ItemArray[0]));
+                            codigo_pais = nueva.Quitar_espacios(Convert.ToString(codigopais.Rows[0].ItemArray[0]));
+                        }
+
+                        DataTable id_cartaporte = nueva.Consulta("Select cartas_de_porte.llave from cartas_final inner join cartas_de_porte on cartas_final.llave=cartas_de_porte.llave where cartas_final.llave = " + id);
+                        string comando = "UPDATE cartas_de_porte SET fecha_modificacion=NOW(),codigo_pais='" + codigo_pais + "',numero_cartaporte='" + numero + "',c4='" + c4 + "',c5='" + c5 + "',c6='" + c6 + "',c7='" + c7 + "',C8='" + c8 + "',c9='" + c9 + "" +
+                            "',c10='" + c10 + "',c11='" + c11 + "',c12='" + c12 + "' WHERE llave=" + nueva.Quitar_espacios(Convert.ToString(id_cartaporte.Rows[0].ItemArray[0])) + "";
+                        nueva.comando(comando);
+
+                        numero_final_para_enviar_al_manifiesto = numero;
+                    }
+                    DataTable cartas = nueva.Consulta("Select llave from cartas_de_porte");
+
+                    if (id == "-1")
+                    {
+
+                        string comando4 = "INSERT INTO Organizaciones_en_cartaportes " +
+                            "VALUES('" + nueva.Quitar_espacios(Convert.ToString(cartas.Rows[cartas.Rows.Count - 1].ItemArray[0])) + "','" + codigo_emisor + "','EMISOR')";
+                        nueva.comando(comando4);
+
+                        string comando5 = "INSERT INTO Organizaciones_en_cartaportes " +
+                       "VALUES('" + nueva.Quitar_espacios(Convert.ToString(cartas.Rows[cartas.Rows.Count - 1].ItemArray[0])) + "','" + codigo_receptor + "','RECEPTOR')";
+                        nueva.comando(comando5);
+
+                    }
                     else
-                        numero = numeroPE;
+                    {
+                        DataTable id_cartaporte = nueva.Consulta("Select cartas_de_porte.llave from cartas_final inner join cartas_de_porte on cartas_final.llave=cartas_de_porte.llave  where cartas_final.llave = " + id);
+                        string comando4 = "UPDATE Organizaciones_en_cartaportes SET id_organizacion='" + codigo_emisor + "' Where id_carta =" + id_cartaporte.Rows[0].ItemArray[0] + " and" +
+                            " papel_organizacion='EMISOR'";
+                        nueva.comando(comando4);
 
-                    DataTable codigopais = nueva.Consulta("Select cartas_de_porte.codigo_pais from cartas_final inner join cartas_de_porte on cartas_final.llave=cartas_de_porte.llave where cartas_final.llave = " + id + "");
-                    if(nueva.Quitar_espacios(Convert.ToString(codigopais.Rows[0].ItemArray[0]))==comboBox1.Text)
-                    {                    
-                        DataTable numero_cartaporte = nueva.Consulta("Select cartas_de_porte.numero_cartaporte from cartas_final inner join cartas_de_porte on cartas_final.llave=cartas_de_porte.llave where cartas_final.llave = " + id + "");
-                        numero = nueva.Quitar_espacios(Convert.ToString(numero_cartaporte.Rows[0].ItemArray[0]));
-                        codigo_pais=nueva.Quitar_espacios(Convert.ToString(codigopais.Rows[0].ItemArray[0]));
+                        string comando5 = "UPDATE Organizaciones_en_cartaportes SET id_organizacion='" + codigo_receptor + "' Where id_carta =" + id_cartaporte.Rows[0].ItemArray[0] + " and" +
+                            " papel_organizacion='RECEPTOR'";
+                        nueva.comando(comando5);
                     }
 
-                    DataTable id_cartaporte = nueva.Consulta("Select cartas_de_porte.llave from cartas_final inner join cartas_de_porte on cartas_final.llave=cartas_de_porte.llave where cartas_final.llave = " + id );
-                    string comando = "UPDATE cartas_de_porte SET fecha_modificacion=NOW(),codigo_pais='"+codigo_pais+"',numero_cartaporte='"+numero+"',c4='" + c4 + "',c5='" + c5 + "',c6='" + c6 + "',c7='"+c7+"',C8='" + c8 + "',c9='" + c9 + "" +
-                        "',c10='" + c10 + "',c11='" + c11 + "',c12='" + c12 + "' WHERE llave=" + nueva.Quitar_espacios(Convert.ToString(id_cartaporte.Rows[0].ItemArray[0])) + "";
-                    nueva.comando(comando);
+                    if (id == "-1")
+                    {
+                        string comando6 = "INSERT INTO cartas_final (id_carta, c13_1,c13_2,c14,c15,c16,c17_1,c17_2,c17_3,c17_4,c17_5,c17_6,c17_7,c17_8,c17_9,c17_10,c17_11,c17_12,c18,c19,c21,c22,dian) " +
+                        " VALUES('" + cartas.Rows[cartas.Rows.Count - 1].ItemArray[0] + "','" + c13_1 + "','" + c13_2 + "','" +
+                        c14 + "','" + c15 + "','" + c16 + "','" + c17_1 + "','" + c17_2 + "','" + c17_3 + "','" + c17_4 + "','" + c17_5 + "','" + c17_6 + "','" +
+                        c17_7 + "','" + c17_8 + "','" + c17_9 + "','" + c17_10 + "','" + c17_11 + "','" + c17_12 + "','" + c18 + "','" + c19 + "','" + c21 + "','" + c22 + "','" + dian + "')";
+                        nueva.comando(comando6);
 
-                    numero_final_para_enviar_al_manifiesto = numero;
-                }
-                DataTable cartas = nueva.Consulta("Select llave from cartas_de_porte");
+                        DataTable cartasFinal = nueva.Consulta("Select llave from cartas_final");
+                        id = cartasFinal.Rows[cartasFinal.Rows.Count - 1].ItemArray[0].ToString();
+                        label34.Text = "EDITANDO CARTA DE PORTE";
+                    }
+                    else
+                    {
+                        string comando6 = "UPDATE cartas_final SET c13_1='" + c13_1 + "',dian='" + dian + "',c13_2='" + c13_2 + "',c14='" + c14 + "'," +
+                            "c15='" + c15 + "',c16='" + c16 + "',c17_1='" + c17_1 + "',c17_2='" + c17_2 + "',c17_3='" + c17_3 + "',c17_4='" + c17_4 + "'," +
+                            "c17_5='" + c17_5 + "',c17_6='" + c17_6 + "',c17_7='" + c17_7 + "',c17_8='" + c17_8 + "',c17_9='" + c17_9 + "',c17_10='" + c17_10 + "'" +
+                            ",c17_11='" + c17_11 + "',c17_12='" + c17_12 + "',c18='" + c18 + "',c19='" + c19 + "',c21='" + c21 + "',c22='" + c22 + "'" +
+                            "WHERE llave=" + id;
+                        nueva.comando(comando6);
 
-                if (id == "-1")
-                {
-
-                    string comando4 = "INSERT INTO Organizaciones_en_cartaportes " +
-                        "VALUES('" + nueva.Quitar_espacios(Convert.ToString(cartas.Rows[cartas.Rows.Count - 1].ItemArray[0])) + "','" + codigo_emisor + "','EMISOR')";
-                    nueva.comando(comando4);
-
-                    string comando5 = "INSERT INTO Organizaciones_en_cartaportes " +
-                   "VALUES('" + nueva.Quitar_espacios(Convert.ToString(cartas.Rows[cartas.Rows.Count - 1].ItemArray[0])) + "','" + codigo_receptor + "','RECEPTOR')";
-                    nueva.comando(comando5);
-
-                }
-                else
-                {
-                    DataTable id_cartaporte = nueva.Consulta("Select cartas_de_porte.llave from cartas_final inner join cartas_de_porte on cartas_final.llave=cartas_de_porte.llave  where cartas_final.llave = " + id );
-                    string comando4 = "UPDATE Organizaciones_en_cartaportes SET id_organizacion='" + codigo_emisor + "' Where id_carta =" + id_cartaporte.Rows[0].ItemArray[0] + " and" +
-                        " papel_organizacion='EMISOR'";
-                    nueva.comando(comando4);
-
-                    string comando5 = "UPDATE Organizaciones_en_cartaportes SET id_organizacion='" + codigo_receptor + "' Where id_carta =" + id_cartaporte.Rows[0].ItemArray[0] + " and" +
-                        " papel_organizacion='RECEPTOR'";
-                    nueva.comando(comando5);
-                }
-
-                if (id == "-1")
-                {
-                    string comando6 = "INSERT INTO cartas_final (id_carta, c13_1,c13_2,c14,c15,c16,c17_1,c17_2,c17_3,c17_4,c17_5,c17_6,c17_7,c17_8,c17_9,c17_10,c17_11,c17_12,c18,c19,c21,c22,dian) " +
-                    " VALUES('" + cartas.Rows[cartas.Rows.Count - 1].ItemArray[0] + "','" + c13_1 + "','" + c13_2 + "','" +
-                    c14 + "','" + c15 + "','" + c16 + "','" + c17_1 + "','" + c17_2 + "','" + c17_3 + "','" + c17_4 + "','" + c17_5 + "','" + c17_6 + "','" +
-                    c17_7 + "','" + c17_8 + "','" + c17_9 + "','" + c17_10 + "','" + c17_11 + "','" + c17_12 + "','" + c18 + "','" + c19 + "','" + c21 + "','" + c22 + "','"+dian+"')";
-                    nueva.comando(comando6);
-
-                    DataTable cartasFinal = nueva.Consulta("Select llave from cartas_final");
-                    id = cartasFinal.Rows[cartasFinal.Rows.Count - 1].ItemArray[0].ToString();
-                    label34.Text = "EDITANDO CARTA DE PORTE";
+                    }
+                    MessageBox.Show("Se ha guardado exitosamente");
                 }
                 else
                 {
-                    string comando6 = "UPDATE cartas_final SET c13_1='" + c13_1 + "',dian='" + dian + "',c13_2='" + c13_2 + "',c14='" + c14 + "'," +
-                        "c15='" + c15 + "',c16='" + c16 + "',c17_1='" + c17_1 + "',c17_2='" + c17_2 + "',c17_3='" + c17_3 + "',c17_4='" + c17_4 + "'," +
-                        "c17_5='" + c17_5 + "',c17_6='" + c17_6 + "',c17_7='" + c17_7 + "',c17_8='" + c17_8 + "',c17_9='" + c17_9 + "',c17_10='" + c17_10 + "'" +
-                        ",c17_11='" + c17_11 + "',c17_12='" + c17_12 + "',c18='" + c18 + "',c19='" + c19 + "',c21='" + c21 + "',c22='" + c22 + "'" +
-                        "WHERE llave=" + id;
-                    nueva.comando(comando6);
-
+                    MessageBox.Show("No puede dejar el campo 2 y 3 vacíos, además se tiene que ingresar el código del país ", "Advertencia Ingreso de datos", MessageBoxButtons.OK, MessageBoxIcon.Warning);
                 }
-                MessageBox.Show("Se ha guardado exitosamente");
-            }
-            else
-            {
-                MessageBox.Show("No puede dejar el campo 2 y 3 vacíos, además se tiene que ingresar el código del país ","Advertencia Ingreso de datos",MessageBoxButtons.OK,MessageBoxIcon.Warning);
-            }
 
-            
+
             
         }
     }
